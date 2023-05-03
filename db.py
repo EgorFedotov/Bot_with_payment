3847e30bc8b9f7edf91f9446e683c0dbfb0cbf0c
EgorFedotov <egorfedotovarz@gmail.com>
2023-05-03 10:40:55 +0300

    added db

---
+++ db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+
+    def user_exists(self, user_id):
+        """Проверка на существовние пользователя."""
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_user(self, user_id):
+        """Создание пользователя в БД."""
+        with self.connection:
+            self.cursor.execute("INSERT INTO `users` (`user_id`) VALUES (?)", (user_id,))
+
+    def user_money(self, user_id):
+        """Баланс пользователя."""
+        with self.connection:
+            result = self.cursor.execute("SELECT `money` FROM `users` WHERE `user_id` = ?", (user_id,)).fetchmany(1)
+            return int(result[0][0])
+
+    def set_money(self, user_id, money):
+        """Изменение баланса пользователя."""
+        with self.connection:
+            return self.cursor.execute("UPDATE `users` SET `money` = ? WHERE `user_id` = ?", (money, user_id))
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 
+from db import Database
+
+
 load_dotenv()
 
 
@@ -17,6 +20,8 @@ HELP_COMMAND = """
 bot = Bot(TOKEN_API)
 dispather = Dispatcher(bot)
 
+db = Database("database.db")
+
 ikb = InlineKeyboardMarkup(row_width=2)
 ikbPay = InlineKeyboardButton(text='Пополнить баланс',
                               url='https://github.com/EgorFedotov')
@@ -29,6 +34,8 @@ async def on_startup(_):
 
 @dispather.message_handler(commands=['start'])
 async def start_command(message: types.Message):
+    if not db.user_exists(message.from_user.id):
+        db.add_user(message.from_user.id)
     await message.answer(text=f'Привет, {message.from_user.username}')
     await message.answer(text='Я - бот для пополнения баланса. Нажмите на кнопку, чтобы пополнить баланс',
                          reply_markup=ikb)
